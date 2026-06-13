8309abb8b1395ccecf5b3f871887743fa8638f84
Aditya Shrestha <yours.only.adi@gmail.com>
2026-06-13 23:58:19 +0545

    added notification

--- api/main.py
+++ api/main.py
@@ -19,6 +19,9 @@ Run with:
 from contextlib import asynccontextmanager
 import logging
 import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
